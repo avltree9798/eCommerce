fc08ad435c23ee20e2ab8905b1595287fc70d0ea
Javatar <anthonyviriya98@gmail.com>
2016-02-19 21:19:15 +0000

    Add cart

---
+++ assets/image.php
@@ -0,0 +1,19 @@
+<?php
+// Load the stamp and the photo to apply the watermark to
+$filename = 'images/'.$_GET["img"];
+$ext=substr($filename ,strrpos($filename ,'.')+1);
+$im = "";
+if(strtolower($ext)=='png'){
+    $im = imagecreatefrompng($filename);
+}else{
+    $im = imagecreatefromjpeg($filename);
+}
+$stamp = imagecreatefrompng('stamp.png');
+$marge_right = 10;
+$marge_bottom = 10;
+$sx = imagesx($stamp);
+$sy = imagesy($stamp);
+imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
+header('Content-type: image/png');
+imagepng($im);
+imagedestroy($im);
