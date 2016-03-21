9020eb1567e85a9489b35edfa602aaf52950dec7
Javatar <anthonyviriya98@gmail.com>
2016-03-21 13:41:58 +0000

    Search

--- index.php
+++ index.php
@@ -9,9 +9,50 @@ require 'lib/Functions.php';
 include 'controller/config.php';
 //$query = "SELECT d.ProductName, c.Name, u.Unit, s.Stock, pp.Price FROM design d JOIN products p ON d.Id = p.DesignID JOIN categories c ON p.CategoryID = c.Id, units u, stock s, productsprice pp
 //WHERE c.Id = u.CategoryID AND s.ProductID = p.Id AND s.UnitID = u.Id AND pp.ProductID = p.Id AND pp.isCurrent = 1";
-$query = "SELECT p.Id, p.ProductName, p.ProductImage, c.Name, pp.Price, p.CategoryID FROM design d JOIN products p ON d.Id = p.DesignID JOIN categories c ON p.CategoryID = c.Id, productsprice pp WHERE pp.ProductID = p.Id AND pp.isCurrent = 1  AND p.isDeleted = 0";
-$result = mysql_query($query);
+$c = $_GET['category'];
+$query="";
+if(isset($_GET['search'])){
+    $cari=$_GET['search'];
+    if(isset($_GET['category'])) {
+        if($c!='0'){
+            $query = "SELECT p.Id, p.ProductName, p.ProductImage, c.Name, pp.Price, p.CategoryID FROM design d JOIN products p ON d.Id = p.DesignID JOIN categories c ON p.CategoryID = c.Id, productsprice pp WHERE pp.ProductID = p.Id AND pp.isCurrent = 1  AND p.isDeleted = 0 AND (p.ProductName LIKE '%$cari%' OR p.Description LIKE '%$cari%' OR d.ProductName LIKE '%$cari%') AND c.Id=$c";
+        }else{
+            $query = "SELECT p.Id, p.ProductName, p.ProductImage, c.Name, pp.Price, p.CategoryID FROM design d JOIN products p ON d.Id = p.DesignID JOIN categories c ON p.CategoryID = c.Id, productsprice pp WHERE pp.ProductID = p.Id AND pp.isCurrent = 1  AND p.isDeleted = 0 AND (p.ProductName LIKE '%$cari%' OR p.Description LIKE '%$cari%' OR d.ProductName LIKE '%$cari%')";
+        }
+    }else{
+        $query = "SELECT p.Id, p.ProductName, p.ProductImage, c.Name, pp.Price, p.CategoryID FROM design d JOIN products p ON d.Id = p.DesignID JOIN categories c ON p.CategoryID = c.Id, productsprice pp WHERE pp.ProductID = p.Id AND pp.isCurrent = 1  AND p.isDeleted = 0 AND (p.ProductName LIKE '%$cari%' OR p.Description LIKE '%$cari%' OR d.ProductName LIKE '%$cari%')";
+    }
+}else{
+    if(isset($_GET['category'])) {
+        if ($c != 0) {
+            $query = "SELECT p.Id, p.ProductName, p.ProductImage, c.Name, pp.Price, p.CategoryID FROM design d JOIN products p ON d.Id = p.DesignID JOIN categories c ON p.CategoryID = c.Id, productsprice pp WHERE pp.ProductID = p.Id AND pp.isCurrent = 1  AND p.isDeleted = 0 AND c.Id=$c";
+        }
+    }else{
+        $query = "SELECT p.Id, p.ProductName, p.ProductImage, c.Name, pp.Price, p.CategoryID FROM design d JOIN products p ON d.Id = p.DesignID JOIN categories c ON p.CategoryID = c.Id, productsprice pp WHERE pp.ProductID = p.Id AND pp.isCurrent = 1  AND p.isDeleted = 0";
+    }
+}
+$result = mysql_query($query) or die(mysql_error());
 $rate = new exchange_rates();
+?>
+
+    <form>
+        <input type="text" name="search" placeholder="Search Product"/>
+        <select name="category">
+            <?php
+            $queryCat = "SELECT * FROM categories";
+            $resCat = mysql_query($queryCat);
+
+            echo '<option value="0">All Category</option>';
+            while($row=mysql_fetch_array($resCat)){
+                $catName = $row["Name"];
+                $catId = $row["Id"];
+                echo "<option value='$catId'>$catName</option>";
+            }
+            ?>
+        </select>
+        <input type="submit" value="Search"/>
+    </form>
+<?php
 while($row=mysql_fetch_array($result)){
     $pID = $row["Id"];
     ?>
@@ -34,7 +75,7 @@ while($row=mysql_fetch_array($result)){
             <tr>
                 <td>Price</td>
                 <td>:</td>
-                <td><?php echo 'Rp. '.$row["Price"]." or $".$rate->exchange_rate_convert("IDR","USD",$row["Price"]); ?></td>
+                <td><?php //echo 'Rp. '.$row["Price"]." or $".$rate->exchange_rate_convert("IDR","USD",$row["Price"]); ?></td>
             </tr>
             <tr>
                 <?php
